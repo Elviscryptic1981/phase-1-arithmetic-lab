655f55aea8e5b250894d76f9d1dc44a72337beb6
Elvis <Elviskimani99@gmail.com>
2024-06-26 11:52:33 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() *100) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4
+
+const max = Math.max(1, 3, 7, 10, 20);
+
+module.exports = {
+    num1,
+    num2,
+    multiply,
+    random,
+    num3,
+    num4,
+    mod,
+    max,
+};
